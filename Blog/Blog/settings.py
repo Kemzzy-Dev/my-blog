1fd1fe6034677ef16996eabe307ab9011e2fa4c3
Ekemini Udongwo <kemzzy17@gmail.com>
2023-05-18 17:57:38 +0100

    Update settings.py

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -209,7 +209,11 @@ DATABASES = {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
