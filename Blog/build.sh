acc24cf98190fa6905950caa4157f43e14d7f06a
Kemzzy-Dev <kemzzy17@gmail.com>
2023-09-14 07:17:04 +0100

    edited build.sh

--- Blog/build.sh
+++ Blog/build.sh
@@ -5,5 +5,5 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py createsuperuser --no-input --username admin --password 2fK9f@example.com 
+python manage.py createsuperuser --no-input --username admin --email kemzzy17@gmail.com 
 python manage.py migrate
